d5c839c0e17298f15b1113688521a6efea7bb90c
rronik3 <veronikazubko891@gmail.com>
2025-04-17 11:36:10 +0200

    done

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -3,10 +3,9 @@ package test;
 import domain.Exercise;
 
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int luckyTickets = Exercise.Calculate();
+        System.out.println("The number of lucky streetcar tickets: " + luckyTickets);
     }
 }
+
